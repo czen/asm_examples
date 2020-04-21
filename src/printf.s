ef5877d78a19ec939c3872f7b6b211f113d0e862
czen <taccessviolation@gmail.com>
2020-04-21 19:15:16 +0000

    ADD: printf comments

--- src/printf.s
+++ src/printf.s
@@ -5,7 +5,8 @@ format: .asciz "%d\n"
     .global main
 main:
   push %rbx
-  lea  format(%rip), %rdi
+  lea  format(%rip), %rdi # load efective address 
+                          # instead of writing mov $format ...
   mov  $21, %esi           # Writing to ESI zero extends to RSI.
   xor %eax, %eax          # Zeroing EAX is efficient way to clear AL.
   call printf
